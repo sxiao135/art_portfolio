2ae57d9bdf8292600bb33f1a559af90342b841df
sxiao135 <133243907+sxiao135@users.noreply.github.com>
2023-11-23 15:44:00 -0600

    trying to hover/change

---
+++ ARTPF/PORTFOLIO/templates/portfolio/hover.js
@@ -0,0 +1,11 @@
+var iframe = document.getElementById('if');
+var oldSrc = iframe.src;
+var newSrc = 'https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_onmouseover_dom';
+
+iframe.addEventListener('mouseover', function() {
+    iframe.src = newSrc;
+});
+
+iframe.addEventListener('mouseout', function() {
+    iframe.src = oldSrc;
+});
