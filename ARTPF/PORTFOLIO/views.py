450943da22794fb5f4f2b5b37b01b643a448768d
sxiao135 <133243907+sxiao135@users.noreply.github.com>
2023-11-22 21:54:02 -0600

    add list, add artwork folder

--- ARTPF/PORTFOLIO/views.py
+++ ARTPF/PORTFOLIO/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Works
 
 # Create your views here.
 def testing(request):
     return HttpResponse("Hello World!")
 
 def homepage(request):
-    return render(request, "portfolio/hpart.html")
+    return render(request, "portfolio/hpart.html", {
+        "works": Works.objects.all()
+    })
