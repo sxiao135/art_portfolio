80d58eea5d3c31646f7cbb36cc6b9a6dba92ba37
sxiao135 <133243907+sxiao135@users.noreply.github.com>
2023-11-23 16:58:24 -0600

    eits to appear on git

--- ARTPF/PORTFOLIO/urls.py
+++ ARTPF/PORTFOLIO/urls.py
@@ -20,5 +20,5 @@ from . import views
 
 urlpatterns = [
     path('', views.testing, name="testing"),
-    path('homepage', views.homepage, name="homepage"),
+    path('index', views.homepage, name="homepage"),
 ]
