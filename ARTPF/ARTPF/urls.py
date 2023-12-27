a2e40305fabadc94a3c860f2107703149588dd8a
sxiao135 <133243907+sxiao135@users.noreply.github.com>
2023-12-27 14:54:50 -0600

    issues with static file. paths must be fixed for images

--- ARTPF/ARTPF/urls.py
+++ ARTPF/ARTPF/urls.py
@@ -22,4 +22,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('portfolio/', include("portfolio.urls")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+if settings.DEBUG: 
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
