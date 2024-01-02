6893b3daf59f2b461b00df284172236e917a21c4
sxiao135 <133243907+sxiao135@users.noreply.github.com>
2024-01-02 12:12:36 -0600

    add db

--- ARTPF/ARTPF/settings.py
+++ ARTPF/ARTPF/settings.py
@@ -77,10 +77,17 @@ WSGI_APPLICATION = 'ARTPF.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    { 'default': { 
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+        'HOST': os.environ.get("DB_HOST"), 
+        'NAME': os.environ.get("DB_NAME"), 
+        'USER': os.environ.get("DB_USER"), 
+        'PASSWORD':os.environ.get("DB_PASSWORD"), 
+        'PORT':os.environ.get("DB_PORT"), } }
 }
 
 
