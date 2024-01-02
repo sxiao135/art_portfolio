b48fa59808f67bd4cd34969c0249881fea8b234b
sxiao135 <133243907+sxiao135@users.noreply.github.com>
2024-01-02 13:46:02 -0600

    Update settings.py

--- ARTPF/ARTPF/settings.py
+++ ARTPF/ARTPF/settings.py
@@ -83,13 +83,14 @@ key = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
-    { 'default': { 
+    'default': { 
         'ENGINE': 'django.db.backends.postgresql_psycopg2', 
         'HOST': os.environ.get("DB_HOST"), 
         'NAME': os.environ.get("DB_NAME"), 
         'USER': os.environ.get("DB_USER"), 
         'PASSWORD':os.environ.get("DB_PASSWORD"), 
-        'PORT':os.environ.get("DB_PORT"), } }
+        'PORT':os.environ.get("DB_PORT"), 
+    }
 }
 DATABASES = frozenset(key.items())
 
