14961c52be4039940605a55d93651987591661de
sxiao135 <133243907+sxiao135@users.noreply.github.com>
2024-01-02 11:45:13 -0600

    edits to files

--- ARTPF/ARTPF/settings.py
+++ ARTPF/ARTPF/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t)(ca-xqk$okhn^4y#dhsiie0q0_$lv1eg90$g^&a=p=5$imx)'
+SECRET_KEY = os.environ.get('SECRET_KEY', '(django-insecure-t)(ca-xqk$okhn^4y#dhsiie0q0_$lv1eg90$g^&a=p=5$imx)')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = str(os.environ.get('DEBUG')) == '1'
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh'] #
+if not DEBUG:
+    ALLOWED_HOSTS += [os.environ.get('ALLOWED_HOST')]
 
 
 # Application definition
