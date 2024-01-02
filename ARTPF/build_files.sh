bb5d7b29648dd926a8d816eeb68d1a99f4050ead
sxiao135 <133243907+sxiao135@users.noreply.github.com>
2024-01-02 13:32:03 -0600

    Update build_files.sh

--- ARTPF/build_files.sh
+++ ARTPF/build_files.sh
@@ -1,4 +1,6 @@
 pip install -r requirements.txt 
-make migrations python3.9 manage.py migrate python3.9 manage.py collectstatic
+python3.9 manage.py make migrations 
+python3.9 manage.py migrate 
+python3.9 manage.py collectstatic
 
 #TESTING
