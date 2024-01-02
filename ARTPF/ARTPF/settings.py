c129257cb7123e9e769f1162517c43b4afb3a83e
sxiao135 <133243907+sxiao135@users.noreply.github.com>
2024-01-02 13:19:38 -0600

    Update settings.py

--- ARTPF/ARTPF/settings.py
+++ ARTPF/ARTPF/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '(django-insecure-t)(ca-xqk$okhn^4y#dhsiie0q0_$lv1eg90$g^&a=p=5$imx)')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == '1'
