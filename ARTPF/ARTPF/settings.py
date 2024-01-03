51acce8c9789dd6acf04d356517a888b71a55354
sxiao135 <133243907+sxiao135@users.noreply.github.com>
2024-01-02 19:41:14 -0600

    edit debug and environ

--- ARTPF/ARTPF/settings.py
+++ ARTPF/ARTPF/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get('DEBUG')) == '1'
+DEBUG = False
+#DEBUG = str(os.environ.get('DEBUG')) == '1'
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh'] #
 if not DEBUG:
