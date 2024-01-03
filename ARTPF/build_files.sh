8777eaae0585ccac38adccfe4a87e73606415580
sxiao135 <133243907+sxiao135@users.noreply.github.com>
2024-01-02 19:07:03 -0600

    Update build_files.sh addnoinput

--- ARTPF/build_files.sh
+++ ARTPF/build_files.sh
@@ -1,6 +1,6 @@
 pip install -r requirements.txt 
 python3.9 manage.py make migrations 
 python3.9 manage.py migrate 
-python3.9 manage.py collectstatic
+python3.9 manage.py collectstatic --noinput
 
 #TESTING
