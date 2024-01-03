69d424c1bd79f2ec535e990e01de2ea895fdf1b0
sxiao135 <133243907+sxiao135@users.noreply.github.com>
2024-01-02 21:19:22 -0600

    edit functions

--- ARTPF/portfolio/urls.py
+++ ARTPF/portfolio/urls.py
@@ -21,9 +21,12 @@ from . import views
 urlpatterns = [
     path('', views.testing, name="testing"),
     path('about', views.about, name="about"),
-    path('homepage', views.portfolio_page, {'tag': 'fp'}, name="homepage"),
-    path('studies', views.portfolio_page, {'tag': 'study'}, name="studies"),
-    path('digital', views.portfolio_page, {'tag': 'digital'}, name="digital"),
+    path('homepage', views.homepage, name="homepage"),
+    path('studies', views.studies, name="studies"),
+    path('digital', views.digital, name="digital"),
+    # path('homepage', views.portfolio_page, {'tag': 'fp'}, name="homepage"), #for the portfolio_pge function
+    # path('studies', views.portfolio_page, {'tag': 'study'}, name="studies"),
+    # path('digital', views.portfolio_page, {'tag': 'digital'}, name="digital"),
     # path('studies/<int:image_id>/', views.image_detail, name='image_detail'), #these are repetitive, not sure how to fix yet
     # path('digital/<int:image_id>/', views.image_detail, name='image_detail'),
     # path('homepage/<int:image_id>/', views.image_detail, name='image_detail'),
--- ARTPF/portfolio/views.py
+++ ARTPF/portfolio/views.py
@@ -12,51 +12,51 @@ def about(request):
 def error(request):
     return render(request, "portfolio/error.html")
 
-def portfolio_page(request, tag):
-    tag_mapping = {
-        'fp': 1,
-        'study': 2,
-        'digital': 6,
-    }
-    print(tag)
+# def portfolio_page(request, tag):
+#     tag_mapping = {
+#         'fp': 1,
+#         'study': 2,
+#         'digital': 6,
+#     }
+#     print(tag)
 
-    tag_id = tag_mapping.get(tag, None)
-    tag_id = int(tag_id)
-    if tag_id is None:
-        # Handle invalid tag, you can raise a 404 or redirect to a default page
-        return render(request, "portfolio/error.html")
+#     tag_id = tag_mapping.get(tag, None)
+#     tag_id = int(tag_id)
+#     if tag_id is None:
+#         # Handle invalid tag, you can raise a 404 or redirect to a default page
+#         return render(request, "portfolio/error.html")
     
-    # print(tag_id)
-    objects_with_tag = Works.objects.filter(tags=tag_id) #.prefetch_related_related(Tag)
-    # print(f"Tag ID: {tag_id}, Works count: {objects_with_tag.count()}")
+#     # print(tag_id)
+#     objects_with_tag = Works.objects.filter(tags=tag_id) #.prefetch_related_related(Tag)
+#     # print(f"Tag ID: {tag_id}, Works count: {objects_with_tag.count()}")
     
-    template_name = f"portfolio/{tag}.html"
+#     template_name = f"portfolio/{tag}.html"
 
-    context = {
-        "works": objects_with_tag
-    }
+#     context = {
+#         "works": objects_with_tag
+#     }
 
-    return render(request, template_name, context)
+#     return render(request, template_name, context)
 
 # def image_detail(request, image_id):
 #     image = Works.objects.get(id=image_id)
 #     return render(request, 'portfolio/image_detail.html', {'image':image})
 #
 #####OLD FUNCTIONS
-# def homepage(request):
-#     objects_with_tag_fp = Works.objects.filter(tags=1) #only works with id numbers
-#     return render(request, "portfolio/hpart.html", { #renders the first page with the studies
-#         "works": objects_with_tag_fp #include sql/django table with the titles and img_paths
-#     })
+def homepage(request):
+    objects_with_tag_fp = Works.objects.filter(tags=1) #only works with id numbers
+    return render(request, "portfolio/fp.html", { #renders the first page with the studies
+        "works": objects_with_tag_fp #include sql/django table with the titles and img_paths
+    })
 
-# def studies(request):
-#     objects_with_tag_study = Works.objects.filter(tags=2) #only works with id numbers
-#     return render(request, "portfolio/studies.html", { #renders the first page with the studies
-#         "studies": objects_with_tag_study #include sql/django table with the titles and img_paths
-#     })
+def studies(request):
+    objects_with_tag_study = Works.objects.filter(tags=2) #only works with id numbers
+    return render(request, "portfolio/study.html", { #renders the first page with the studies
+        "studies": objects_with_tag_study #include sql/django table with the titles and img_paths
+    })
 
-# def digital(request):
-#     objects_with_tag_digital = Works.objects.filter(tags=4) #only works with id numbers
-#     return render(request, "portfolio/digital.html", { #renders the first page with the studies
-#         "works": objects_with_tag_digital #include sql/django table with the titles and img_paths
-#     })
+def digital(request):
+    objects_with_tag_digital = Works.objects.filter(tags=6) #only works with id numbers
+    return render(request, "portfolio/digital.html", { #renders the first page with the studies
+        "works": objects_with_tag_digital #include sql/django table with the titles and img_paths
+    })
