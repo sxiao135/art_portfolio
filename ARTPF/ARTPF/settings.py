9dd53cbdf4c10dc4b27be6f15b6798792bb2e1b0
sxiao135 <133243907+sxiao135@users.noreply.github.com>
2024-01-05 16:54:11 -0600

    edits

--- ARTPF/ARTPF/settings.py
+++ ARTPF/ARTPF/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/' #edit
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets') #the absolute path to the directory where app serves static files
 
