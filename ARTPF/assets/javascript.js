3e3f9b1867a9a8513d13b1b2c13b70273ab85697
sxiao135 <133243907+sxiao135@users.noreply.github.com>
2024-01-05 14:40:39 -0600

    remove assets folder

--- ARTPF/assets/javascript.js
+++
@@ -1,9 +0,0 @@
-function showText() {
-    document.getElementById('image').style.display = 'none';
-    document.getElementById('text').style.opacity = '1';
-}
-
-  function showImage() {
-    document.getElementById('image').style.display = 'block';
-    document.getElementById('text').style.opacity = '0';
-}
