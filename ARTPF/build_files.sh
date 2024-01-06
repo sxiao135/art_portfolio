e939bc75e329be162b6846fd19060846640747fd
sxiao135 <133243907+sxiao135@users.noreply.github.com>
2024-01-06 11:42:55 -0600

    edit to requirements

--- ARTPF/build_files.sh
+++ ARTPF/build_files.sh
@@ -1,4 +1,4 @@
-pip install -r requirements.txt 
+python3 -m install -r requirements.txt 
 python3.9 manage.py make migrations 
 python3.9 manage.py migrate 
 python3.9 manage.py collectstatic --noinput
