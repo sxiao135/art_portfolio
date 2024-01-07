0f9f4a49981b9d7fbde5151ecce8061f1bcaadc1
sxiao135 <133243907+sxiao135@users.noreply.github.com>
2024-01-07 10:47:16 -0600

    edit vercel.json and remove database

--- ARTPF/ARTPF/settings.py
+++ ARTPF/ARTPF/settings.py
@@ -82,14 +82,14 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
-    'default': { 
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
-        'HOST': os.environ.get('DB_HOST'), 
-        'NAME': os.environ.get('DB_NAME'), 
-        'USER': os.environ.get('DB_USER'), 
-        'PASSWORD':os.environ.get('DB_PASSWORD'), 
-        'PORT':os.environ.get('DB_PORT'), 
-    }
+    # 'default': { 
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+    #     'HOST': os.environ.get('DB_HOST'), 
+    #     'NAME': os.environ.get('DB_NAME'), 
+    #     'USER': os.environ.get('DB_USER'), 
+    #     'PASSWORD':os.environ.get('DB_PASSWORD'), 
+    #     'PORT':os.environ.get('DB_PORT'), 
+    # }
 }
 
 # Password validation
