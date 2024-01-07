9e4995c9fa75b16ec6fb690834abb0b7b2d8937b
sxiao135 <133243907+sxiao135@users.noreply.github.com>
2024-01-07 10:37:45 -0600

    Update wsgi.py

--- ARTPF/ARTPF/wsgi.py
+++ ARTPF/ARTPF/wsgi.py
@@ -10,4 +10,4 @@ import os
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ARTPF.settings') 
 application=get_wsgi_application()
-#app=application #add here
+app=application #add here
